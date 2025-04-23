4c97f5ad287429a4d6c5366e6604cfacd1e321c6
mohamedobaya <mohamed.ali.obaya@gmail.com>
2025-04-23 07:10:03 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Mohamed Obaya on 23/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
