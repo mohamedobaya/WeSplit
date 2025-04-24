e6778984ab2a06f36ae8d2ff688f6e55687bb05d
mohamedobaya <mohamed.ali.obaya@gmail.com>
2025-04-24 11:06:03 +0200

    Added Sections

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var checkAmount = 0.0
+    @State var numberOfPeople = 2
+    @State var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0 - 2) people")
+                        }
+                    }
+                    .pickerStyle(.)
+                }
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    Text("\(numberOfPeople)")
+                }
+            }
         }
-        .padding()
     }
 }
 
